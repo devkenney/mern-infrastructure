41efa814125154f8c29609939fbb8d21862262b5
Devin Kenney <devin.kenney111@gmail.com>
2022-10-26 16:58:05 -0400

    Deleted a couple file mentions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import { useState } from 'react';
 import Auth from './pages/Auth';
-import NewOrder from './pages/NewOrder';
-import OrderHistory from './pages/OrderHistory';
 import NavBar from './components/NavBar';
-import Test from './pages/Test'
 import { Routes, Route } from 'react-router-dom'
 import { getUser} from './utilities/users-service';
 
@@ -19,9 +16,7 @@ function App() {
           <>
             <NavBar setUser={setUser} user={user} />
             <Routes>
-              <Route path="/orders/new" element={<NewOrder />} />
-              <Route path="/orders" element={<OrderHistory />} />
-              <Route path="/test/:symbol" element={<Test />} />
+
             </Routes>
           </>
         :
